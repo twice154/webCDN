2ec5e035c96ccf6d197dd0e7ee1e9e0797766317
Lee Nam Woo dlskadn123 <terranada@naver.com>
2018-09-04 19:25:18 +0900

    now on handling many rooms

--- server/index.js
+++ server/index.js
@@ -1,1 +1,115 @@
-console.log("HEROKU INITIALIZE COMPLETE")
+//////////////////////////////////////////////////
+/* Module Insertion */
+const path = require("path")
+const http = require("http")
+const express = require("express")
+const socketIO = require("socket.io")
+
+const srcPath = path.join(__dirname, "../src")
+const port = process.env.PORT || 3000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+app.use(express.static(srcPath))
+
+// 선입선출 webCDN 피어배정
+let socketList = {}
+/*
+{
+    room1 : {
+        socket1 : {
+            numOfCurrentPeers : 1
+        },
+        socket2 : {
+            numOfCurrentPeers : 2
+        },
+        ...
+    },
+    room2 : {
+
+    },
+    ...
+}
+*/
+
+//////////////////////////////////////////////////
+// socketIO Connecting
+// Built in event
+io.on("connection", (socket) => {
+    conosole.log(`New client joined server : socket.id=${socket.id}`)
+
+    socket.on("create or join", (room) => {
+        log("Received request to create or join room " + room)
+
+        let clientsInRoom = io.sockets.adapter.rooms[room]
+        let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0
+        log("Room " + room + " now has " + numClients + " client(s)")
+
+        if(numClients === 0) {
+            socket.join(room)
+            log("Client ID(I) " + socket.id + " created room " + room)
+
+            addRoomToList(socketList, room, socket.id)
+
+            socket.emit("created", room)
+        // 일단은 room 하나에 100명까지만 핸들링하도록 한다.
+        } else if(numClients < 100) {
+            log("Client ID(I) " + socket.id + " joined room " + room)
+            socket.join(room)
+
+            addClientToRoom(socketList, room, socket.id)
+
+            socket.broadcast.to(room).emit("join", room)
+            socket.emit("joined", room)
+        // room 하나에 100명 초과되면 full로 더 이상 webCDN 동작X
+        } else {
+            socket.emit("full", room)
+        }
+    })
+    socket.on("requestPeer", (room, ackCallback) => {
+        ackCallback(findPeer(socketList))
+    })
+    // Built in event DISCONNECT
+    socket.on("disconnect", () => {
+        
+    })
+})
+
+//////////////////////////////////////////////////
+// 서버에 포트 할당
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
+
+//////////////////////////////////////////////////
+// Convenience function to log server messages on the client
+function log() {
+    let array = ["Message from server : "]
+    array.push.apply(array, arguments)
+    socket.emit("log", array)
+}
+
+// Adding Room to socketList
+function adddRoomToList(socketList, room, socketID) {
+    socketList[`${room}`] = {}
+    addClientToRoom(socketList, room, socketID)
+}
+// Adding client to room
+function addClientToRoom(socketList, room, socketID) {
+    socketList[room][`${socketID}`] = {}
+    socketList[`${room}`][`${socketID}`].numOfCurrentPeers = 0
+    socketList[`${room}`][`${socketID}`].id = socketID
+}
+
+// For finding idle peers
+function findPeer(socketList) {
+    for(let peerID in socketList[`${room}`]) {
+        if(peerID.numOfCurrentPeers < 3) {
+            // io.sockets.connected[socket.id] : 특정 클라이언트에게만 이벤트를 보내는 방법
+            io.sockets.connected[peerID].emit()
+            return peerID.id
+        }
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,81 @@
-"use strict"
-
 //////////////////////////////////////////////////
 /* Variable Initialize */
-let mySocketID = null
+// One peer handles three peer
+let peer1ID = null
+let sendPeer1Connection = null
+let sendData1Channel = null
 
-let numOfPeerConnection = 0
+let peer2ID = null
+let sendPeer2Connection = null
+let sendData2Channel = null
 
-let peerConnection1 = null
-let dataChannel1 = null
+let peer3ID = null
+let sendPeer3Connection = null
+let sendData3Channel = null
 
-let peerConnection2 = null
-let dataChannel2 = null
-
-let peerConnection3 = null
-let dataChannel3 = null
+let whoSendMeID = null
+let receivePeerConnection = null
+let receiveDataChannel = null
+// Some Constraints
+let peerConnectionConstraint = {
+    "iceServers" : [{
+        "urls" : "stun:stun.l.google.com:19302"
+    },
+    /*
+    {
+        "urls" : "turn:numb.viagenie.ca",
+        "credential" : "123456",
+        "username" : "terranada@naver.com"
+    }*/
+    ]
+}
+let dataChannelConstraint = null
 
 //////////////////////////////////////////////////
 /* Socket.io Initialize */
 // URL주소를 통해서 room 구분
-let room = document.URL
+const room = document.URL
 
-let socket = io()
+const socket = io()
 
 if(room != '') {
     socket.emit("create or join", room)
-    console.log("Attempted to create or join room", room)
+    console.log("Attempted to create or join room : ", room)
 }
-socket.on("created", function(room, id) {
+
+socket.on("created", function(room) {
     console.log("Created room " + room)
-    console.log("You are the initiator of this room " + room)
-    mySocketID = id
-    console.log("My Socket ID is " + mySocketID)
 })
 socket.on("full", function(room) {
-    console.log("Room " + room + " is full")
+    console.log("Room " + room + " is now full. You can't participate webCDN")
 })
 socket.on("join", function(room) {
-    console.log('Another peer made a request to join room ' + room)
+    console.log("Another peer made a request to join room " + room)
 })
-socket.on('joined', function(room, id) {
-    console.log('I joined: ' + room)
-    mySocketID = id
-    console.log("My Socket ID is " + mySocketID)
+socket.on("joined", function(room) {
+    console.log("I joined room : " + room)
+    
+    console.log("Start finding webCDN peer")
+
+    // 서버에게 webCDN 피어 소켓아이디를 요청, ACK으로 받은 ID를 통해서 whoSendMeID로 세팅
+    socket.emit("requestPeer", room, function(otherPeerID) {
+        whoSendMeID = otherPeerID
+    })
 })
-socket.on('log', function(array) {
+// 서버에서 보내는 로그들 받는 이벤트리스너
+socket.on("log", function(array) {
     console.log.apply(console, array)
 })
+
+//////////////////////////////////////////////////
+/* Socket.io Messages <CORE SIGNALING PART> */
+function sendMessage(message) {
+    console.log("Client(I) sending message: ", message);
+    socket.emit("message", message);
+}
+socket.on("message", function(message) {
+    console.log("Client received message : ", message)
+    messageHandling(message)
+})
+
+//////////////////////////////////////////////////
